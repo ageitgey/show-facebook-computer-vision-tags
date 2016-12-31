73cf4690524488fba0679a2b5f7282c49fcc9ade
David Pickart <davidstoreypickart@gmail.com>
2016-12-31 18:48:28 -0500

    More abstract

--- facebook.js
+++ facebook.js
@@ -1,20 +1,18 @@
+const OBSERVER_CONFIG = { attributes: true, childList: true, characterData: false }
 const TAG_PREFIX = 'Image may contain: '
-const FILTERED_TAGS = [
-  'baby',
-  'child'
-]
+const BABY_TAGS = ['baby', 'child']
 
 const hasIntersection = (arrayA, arrayB) => {
   return arrayA.reduce((total, curr) => total || arrayB.includes(curr), false)
 }
 
-const hasBaby = image => {
+const containsTags = (image, filterTags) => {
   const altText = image.alt
   image.setAttribute('data-prev-alt', altText)
   const isCVTag = altText.startsWith(TAG_PREFIX)
   if (!isCVTag) return false
   const tags = altText.slice(TAG_PREFIX.length).split(/, | and /)
-  return hasIntersection(tags, FILTERED_TAGS)
+  return hasIntersection(tags, filterTags)
 }
 
 const getFeedStories = () => {
@@ -25,26 +23,20 @@ const getFeedStories = () => {
 const getStoryImages = story => [...story.getElementsByTagName('img')]
 
 const hideStory = story => {
-  console.log('A baby was hidden!')
+  console.log('A story was hidden!')
   story.setAttribute('hidden', true)
 }
 
-const hideBabies = () => {
-  const stories = getFeedStories()
-  stories.forEach(story => {
-    const images = getStoryImages(story)
-    images.forEach(image => {
-      if (hasBaby(image)) hideStory(story)
-    })
+const hideStoriesWithTags = tags => {
+  getFeedStories().forEach(story => {
+    getStoryImages(story).forEach(image => if (containsTags(image, tags)) hideStory(story))
   })
 }
 
-const observer = new MutationObserver(mutations => {
-  mutations.forEach(mutation => hideBabies())
-})
+const hideBabies = () => hideStoriesWithTags(BABY_TAGS)
 
-const config = { attributes: true, childList: true, characterData: false }
-observer.observe(document.body, config)
+const observer = new MutationObserver(mutations => mutations.forEach(mutation => hideBabies()))
+observer.observe(document.body, OBSERVER_CONFIG)
 
 hideBabies()
 
