42e6b98c6e9a0452b8f64c4cf4db065028d7b7ed
David Pickart <davidstoreypickart@gmail.com>
2016-12-31 18:41:02 -0500

    Working basics

--- facebook.js
+++ facebook.js
@@ -1,14 +1,41 @@
 const TAG_PREFIX = 'Image may contain: '
+const FILTERED_TAGS = [
+  'baby',
+  'child'
+]
+
+const hasIntersection = (arrayA, arrayB) => {
+  return arrayA.reduce((total, curr) => total || arrayB.includes(curr), false)
+}
+
+const hasBaby = image => {
+  const altText = image.alt
+  image.setAttribute('data-prev-alt', altText)
+  const isCVTag = altText.startsWith(TAG_PREFIX)
+  if (!isCVTag) return false
+  const tags = altText.slice(TAG_PREFIX.length).split(/, | and /)
+  return hasIntersection(tags, FILTERED_TAGS)
+}
+
+const getFeedStories = () => {
+  const stories = [...document.querySelectorAll('[data-testid]')]
+  return stories.filter(story => (story.getAttribute('data-testid') === 'fbfeed_story' && !story.getAttribute('hidden')))
+}
+
+const getStoryImages = story => [...story.getElementsByTagName('img')]
+
+const hideStory = story => {
+  console.log('A baby was hidden!')
+  story.setAttribute('hidden', true)
+}
 
 const hideBabies = () => {
-  const images = [...document.getElementsByTagName('img')]
-  images.forEach(el => {
-    if (el.hasAttribute('data-prev-alt') && el.getAttribute('data-prev-alt') === el.getAttribute('alt')) return
-    el.setAttribute('data-prev-alt', el.alt)
-    const altText = el.alt
-    const isCVTag = altText.startsWith(TAG_PREFIX)
-    if (!isCVTag) return
-    const tags = altText.slice(TAG_PREFIX.length).split(/, | and /)
+  const stories = getFeedStories()
+  stories.forEach(story => {
+    const images = getStoryImages(story)
+    images.forEach(image => {
+      if (hasBaby(image)) hideStory(story)
+    })
   })
 }
 
@@ -20,3 +47,4 @@ const config = { attributes: true, childList: true, characterData: false }
 observer.observe(document.body, config)
 
 hideBabies()
+
