e44b2f2113c43d8ae3dac4a2016f41ecf82e48ec
Recep <rahmetsaritekin@gmail.com>
2017-02-10 13:54:31 +0300

    add Locale_tr_TR on facebook.js

--- facebook.js
+++ facebook.js
@@ -91,4 +91,6 @@ if (locale === 'Locale_es_LA') {
   makeLocaleRequest('/locales/vi_VN/messages.json');
 } else if (locale === 'Locale_th_TH') {
   makeLocaleRequest('/locales/th_TH/messages.json');
+} else if (locale === 'Locale_tr_TR') {
+  makeLocaleRequest('/locales/tr_TR/messages.json');
 }
